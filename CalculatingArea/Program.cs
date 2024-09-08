a0d87f8a16b2c127c154c653827e03304a750515
Кирилл Банников <bk27042002.microsoft@gmail.com>
2024-09-08 18:02:29 +0300

    Добавьте файлы проекта.

---
+++ CalculatingArea/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using CalculatingArea;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод радиуса круга с клавиатуры
+        Console.Write("Введите радиус круга: ");
+        double radius = GetValidDoubleInput();
+        IShape circle = new Circle(radius);
+        Console.WriteLine($"Площадь круга: {ShapeAreaCalculator.CalculateShapeArea(circle)}");
+
+        // Ввод сторон треугольника с клавиатуры
+        Console.Write("Введите первую сторону треугольника: ");
+        double sideA = GetValidDoubleInput();
+
+        Console.Write("Введите вторую сторону треугольника: ");
+        double sideB = GetValidDoubleInput();
+
+        Console.Write("Введите третью сторону треугольника: ");
+        double sideC = GetValidDoubleInput();
+
+        try
+        {
+            Triangle triangle = new Triangle(sideA, sideB, sideC);
+            Console.WriteLine($"Площадь треугольника: {ShapeAreaCalculator.CalculateShapeArea(triangle)}");
+            Console.WriteLine($"Треугольник прямоугольный: {triangle.IsRightAngled()}");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+
+    // Метод для получения корректного ввода числа с клавиатуры
+    private static double GetValidDoubleInput()
+    {
+        double value;
+        while (!double.TryParse(Console.ReadLine(), out value) || value <= 0)
+        {
+            Console.WriteLine("Пожалуйста, введите положительное число.");
+        }
+        return value;
+    }
+}
---
+++ CalculatingArea/Triangle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingArea
+{
+    internal class Triangle : IShape
+    {
+        public double SideA { get; }
+        public double SideB { get; }
+        public double SideC { get; }
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                throw new ArgumentException("Стороны треугольника должны быть положительными.");
+            if (sideA + sideB <= sideC || sideA + sideC <= sideB || sideB + sideC <= sideA)
+                throw new ArgumentException("Указанные стороны не могут образовать треугольник.");
+
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+
+        public double CalculateArea()
+        {
+            double semiPerimeter = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - SideA) * (semiPerimeter - SideB) * (semiPerimeter - SideC));
+        }
+
+        // Проверка на прямоугольный треугольник
+        public bool IsRightAngled()
+        {
+            double[] sides = { SideA, SideB, SideC };
+            Array.Sort(sides); // Сортируем стороны, чтобы гипотенуза была последней
+            return Math.Abs(Math.Pow(sides[2], 2) - (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2))) < 0.00001;
+        }
+    }
+}
